a0f2a6a0b6e57db9a3c156a8ffcf3ae16faf103b
Devansh <devansh0112@gmail.com>
2024-02-13 19:17:43 -0500

    e2e integration done

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -3,12 +3,20 @@ import { useLocation } from "react-router-dom";
 import queryString from "query-string";
 import io from "socket.io-client";
 
+import Infobar from "../InfoBar/InfoBar";
+import Input from "../Input/Input";
+import Messages from "../Messages/Messages";
+
+import './Chat.css';
+
 let socket;
 
 const Chat = () => {
   const location = useLocation();
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
   const ENDPOINT = "localhost:5000";
 
   useEffect(() => {
@@ -21,11 +29,45 @@ const Chat = () => {
     setName(name);
     setRoom(room);
 
-    socket.emit('join', { name, room });
+    socket.emit('join', { name, room }, (error) => {
+      if(error) {
+        alert(error);
+      }
+    });
+
+    return () => {
+      // socket.emit('disconnect');
+      socket.off();
+    };
     
   }, [location.search, ENDPOINT]);
 
-  return <h1>Chat</h1>;
+  useEffect(() => {
+    socket.on('message', (message) => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
+
+  // function for sending messages
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    if (message) {
+      socket.emit('sendMessage', message, () => setMessage(""));
+    }
+  };
+
+  console.log(message, messages);
+
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <Infobar room={room} />
+        <Messages messages={messages} name={name} />
+        <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
